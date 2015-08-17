ef9b605aa215a38acfe1bc2a6f450cd741919c7c
YUYINGsa41 <E0005437@u.nus.edu>
2015-08-17 15:56:18 +0800

    EX16

--- CSExercises/Ex16-20/Ex16.cs
+++ CSExercises/Ex16-20/Ex16.cs
@@ -4,7 +4,14 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            System.Console.WriteLine("Please enter your name :");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Please enter your capital letter of genter (M or F assume capital letter):");
+            string CapitalLetterOfGender = System.Console.ReadLine();
+            if (CapitalLetterOfGender == "F")
+                System.Console.WriteLine("Good Morning Ms." + name);
+            else if (CapitalLetterOfGender == "M")
+                System.Console.WriteLine("Good Morning Mr." + name); 
         }
     }
 }

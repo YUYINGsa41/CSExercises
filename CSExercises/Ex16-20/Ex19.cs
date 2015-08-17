41a6aa10fd8a1932f13deb5623f3cd77a699bc3f
YUYINGsa41 <E0005437@u.nus.edu>
2015-08-17 16:57:05 +0800

    EX19

--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -1,10 +1,20 @@
+using System;
 namespace CSExercises
 {
     public class Ex19
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter the distance(/km)");
+            double distance = Convert.ToDouble(Console.ReadLine());
+            if (distance * 1000>=0 && distance * 1000 <= 500)
+                Console.WriteLine("The Cost is:$2.40");
+            else if (distance * 1000 > 500 && distance * 1000 <= 9000)
+                Console.WriteLine("The Cost is:${0:0.00}", (2.40 + 0.04 * (distance * 10 - 5)));
+            else if (distance * 1000 > 9000)
+                Console.WriteLine("The Cost is:${0:0.00}", (2.40 + 0.04 * 85 + 0.05 * (distance * 10 - 90)));
+            else
+                Console.WriteLine("There is an Error");
         }
     }
 }

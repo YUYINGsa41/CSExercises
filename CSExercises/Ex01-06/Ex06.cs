0adef1bc5b5edb3ef3bee97c18dcdfe0d73debfd
YUYINGsa41 <E0005437@u.nus.edu>
2015-08-23 19:20:04 +0800

    EX06

--- CSExercises/Ex01-06/Ex06.cs
+++ CSExercises/Ex01-06/Ex06.cs
@@ -10,7 +10,11 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("enter a number");
+           int a = Convert.ToInt32(Console.ReadLine());
+            double b;
+            b = Math.Sqrt(Convert.ToDouble(a));
+            Console.WriteLine(b);
         }
     }
 }

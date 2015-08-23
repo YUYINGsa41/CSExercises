d0e6455ef0fc4d219d76350a7c9aae37dbf25516
YUYINGsa41 <E0005437@u.nus.edu>
2015-08-23 19:30:09 +0800

    EX08

--- CSExercises/Ex07-15/Ex08.cs
+++ CSExercises/Ex07-15/Ex08.cs
@@ -10,7 +10,10 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("enter a temperature in Centigrade scale");
+            double F = 1.8 * Convert.ToDouble(Console.ReadLine())+32;
+            Console.WriteLine("{0}",F);
+                
         }
     }
 }

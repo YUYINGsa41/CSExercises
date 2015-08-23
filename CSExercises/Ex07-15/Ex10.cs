f830273b209494e68b9f2709b315c5f60e89610f
YUYINGsa41 <E0005437@u.nus.edu>
2015-08-23 19:45:01 +0800

    EX10

--- CSExercises/Ex07-15/Ex10.cs
+++ CSExercises/Ex07-15/Ex10.cs
@@ -10,7 +10,16 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("enter x1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter x2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double Distance = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+            Console.WriteLine(Distance);
         }
     }
 }

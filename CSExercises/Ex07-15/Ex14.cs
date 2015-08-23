d6332154b6c2f475401fb04b79b226c6a8d9ec57
YUYINGsa41 <E0005437@u.nus.edu>
2015-08-23 20:29:09 +0800

    EX14

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,26 @@
+using System;
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("enter a length");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter another length");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter the last length");
+            double c = Convert.ToDouble(Console.ReadLine());
+            if (a + b <= c || a + c <= b || b + c <= a)
+                Console.WriteLine("not a triangle,please enter the length rightly");
+            else
+            {
+                double s = (a + b + c) / 2;
+                double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+                Console.WriteLine(area);
+            }
+
+
         }
     }
 }

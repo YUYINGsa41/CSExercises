a43ff3c256e6cd7930f89a506ef283b3206f4c8e
YUYINGsa41 <E0005437@u.nus.edu>
2015-08-23 20:57:06 +0800

    EX15

--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,10 +1,23 @@
+using System;
 namespace CSExercises
 {
     public class Ex15
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("enter a three-digit number");
+           int  a =Convert.ToInt32( Console.ReadLine());
+            int b=a/100;
+            int c=(a-100*(a/100))/10;
+            int d=(a-100*b-10*c)/1;
+            if (b * b * b + c * c * c + d * d * d == a)
+            {
+                Console.WriteLine("Armstrong number");
+                   
+            }
+              
             
+         
         }
     }
 }
